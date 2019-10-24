9da0c197b1be7d0e8cb7380d0fd3bd4c146a3c1b
Shao-Fu Sung <ssung@squareup.com>
2019-10-24 14:42:54 -0700

    Update package name to squareup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ else:
         long_description = fh.read()
 
 setup(
-    name='square',
+    name='squareup',
     version='3.20191023.0',
     description='Use Square APIs to manage and run business including payment, customer, product, inventory, and employee management.',
     long_description=long_description,
